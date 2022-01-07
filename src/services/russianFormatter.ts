a2143de22a46d7505ce71f1faab9dd2551314047
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-01-07 10:54:35 +0900

    add: service to handle replacements and styles

---
+++ src/services/russianFormatter.ts
@@ -0,0 +1,46 @@
+import {hasRussianCharacter} from '../lib/russian';
+import {findTextNodesUnderElement} from '../lib/node';
+
+const RUSSIAN_STYLES_ID = 'russian-formatter-styles';
+const RUSSIAN_TEXT_CLASSNAME = 'russian-formatter__has-russian';
+
+export function applyStylesToRussianCharacters() {
+  if (document.getElementById(RUSSIAN_STYLES_ID)) {
+    return;
+  }
+  const styles = document.createElement('style');
+  styles.id = RUSSIAN_STYLES_ID;
+  styles.innerHTML = `
+        .${RUSSIAN_TEXT_CLASSNAME} {
+            font-family: system-ui, monospace, sans-serif;
+        }
+    `;
+  document.head.appendChild(styles);
+}
+
+export function replaceRussianTextNodesUnderElement(rootElement: HTMLElement) {
+  const textNodes = findTextNodesUnderElement(rootElement);
+  textNodes.forEach(replaceRussianTextNode);
+}
+
+export function replaceRussianTextNode(node: Text) {
+  if (node.parentElement?.classList.contains(RUSSIAN_TEXT_CLASSNAME)) {
+    return;
+  }
+  const text = node.textContent;
+
+  if (!text) {
+    return;
+  }
+
+  const hasRussian = hasRussianCharacter(text);
+  if (!hasRussian) {
+    return;
+  }
+
+  const replacedTextHTML = `<span class="${RUSSIAN_TEXT_CLASSNAME}">${text}</span>`;
+
+  const newNode = document.createElement('span');
+  newNode.innerHTML = replacedTextHTML;
+  node.replaceWith(newNode);
+}
