a5a47e9c56528ef8bcc96ccd7598121aa5f88e7d
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-01-07 10:53:53 +0900

    add: lib to handle russian related logic

---
+++ src/lib/russian.ts
@@ -0,0 +1,11 @@
+const CYRILLIC_REGEX = /[\u0400-\u04FF]/;
+
+export function hasRussianCharacter(text: string) {
+  return CYRILLIC_REGEX.test(text);
+}
+
+export function isRussianDocument() {
+  const lang = document.documentElement.lang;
+
+  return lang === 'ru';
+}
