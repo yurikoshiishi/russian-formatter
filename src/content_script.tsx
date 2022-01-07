10755a07bc55e346c0e72d645a2a468a9e4a728e
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-01-07 10:54:55 +0900

    add: content script to run service

---
+++ src/content_script.tsx
@@ -0,0 +1,61 @@
+import {observeDOMChange} from './lib/observe';
+import {isRussianDocument} from './lib/russian';
+import {isHTMLElement, isTextNode} from './lib/node';
+import {
+  applyStylesToRussianCharacters,
+  replaceRussianTextNode,
+  replaceRussianTextNodesUnderElement,
+} from './services/russianFormatter';
+
+const observeConfig: MutationObserverInit = {
+  childList: true,
+  characterData: true,
+  subtree: true,
+};
+
+function main() {
+  if (isRussianDocument()) {
+    return;
+  }
+
+  const rootElement = document.body || document.documentElement;
+
+  if (!rootElement) {
+    return;
+  }
+
+  applyStylesToRussianCharacters();
+  replaceRussianTextNodesUnderElement(rootElement);
+
+  observeDOMChange(
+    rootElement,
+    (mutations, observer) => {
+      observer.disconnect();
+
+      mutations.forEach((mutation) => {
+        switch (mutation.type) {
+          case 'characterData':
+            if (isTextNode(mutation.target)) {
+              return replaceRussianTextNode(mutation.target);
+            }
+          case 'childList':
+            if (mutation.addedNodes?.length) {
+              mutation.addedNodes.forEach((node) => {
+                if (isTextNode(node)) {
+                  return replaceRussianTextNode(node);
+                }
+                if (isHTMLElement(node)) {
+                  return replaceRussianTextNodesUnderElement(node);
+                }
+              });
+            }
+        }
+      });
+
+      observer.observe(rootElement, observeConfig);
+    },
+    observeConfig
+  );
+}
+
+main();
